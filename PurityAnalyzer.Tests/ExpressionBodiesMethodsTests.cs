34adab3165964533d9ed491e8947eec9728c3dcf
Yacoub Massad <yacoubmassad@gmail.com>
2018-06-07 14:05:23 +0300

    Adding tests for expression bodied methods

---
+++ PurityAnalyzer.Tests/ExpressionBodiesMethodsTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace PurityAnalyzer.Tests
+{
+    [TestFixture]
+    public class ExpressionBodiesMethodsTests
+    {
+        [Test]
+        public void MethodWithImpureExpressionBodyIsConsideredImpure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public static class Module1
+{
+    static int c = 1;
+    [IsPure]
+    public static int DoSomething() => c++;
+
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+        [Test]
+        public void MethodWithPureExpressionBodyIsConsideredPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething() => 1;
+
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+    }
+}
