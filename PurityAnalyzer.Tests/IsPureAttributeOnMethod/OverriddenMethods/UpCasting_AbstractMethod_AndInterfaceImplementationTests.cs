37783df9d2b7ec69a944d7ee5b8f7839c6151624
Yacoub Massad <yacoubmassad@gmail.com>
2018-06-17 02:00:26 +0300

    More tests (some failing)

---
+++ PurityAnalyzer.Tests/IsPureAttributeOnMethod/OverriddenMethods/UpCasting_AbstractMethod_AndInterfaceImplementationTests.cs
@@ -0,0 +1,1030 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace PurityAnalyzer.Tests.IsPureAttributeOnMethod.OverriddenMethods
+{
+    [TestFixture]
+    public class UpCasting_AbstractMethod_AndInterfaceImplementationTests
+    {
+        [Test]
+        public void UpCastingFromObjectToTypeWithAbstractMethodThatImplementsAnInterfaceMakesMethodImpure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base : IInterface
+{
+    public abstract int Method();
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Base)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+        [Test]
+        public void UpCastingFromObjectToTypeThatOverridesAnAbstractMethodThatImplementsAnInterfaceWithAMethodThatIsImpureKeepsMethodPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base : IInterface
+{
+    public abstract int Method();
+}
+
+public class Derived : Base
+{
+    static int state = 0;
+    public override int Method() => state++;
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+
+        [Test]
+        public void UpCastingFromObjectToTypeThatOverridesAnAbstractMethodWithAMethodThatIsImpureAndThatImplementsAnInterfaceKeepsMethodPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base
+{
+    public abstract int Method();
+}
+
+public class Derived : Base, IInterface
+{
+    static int state = 0;
+    public override int Method() => state++;
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+        [Test]
+        public void UpCastingFromObjectToTypeThatOverridesAnAbstractMethodThatImplementsAnInterfaceWithAMethodThatIsPureMakesMethodImpure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base : IInterface
+{
+    public abstract int Method();
+}
+
+public class Derived : Base
+{
+    public override int Method() => 2;
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+        [Test]
+        public void UpCastingFromObjectToTypeThatOverridesAnAbstractMethodWithAMethodThatIsPureAndThatImplementsAnInterfaceMakesMethodImpure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base
+{
+    public abstract int Method();
+}
+
+public class Derived : Base, IInterface
+{
+    public override int Method() => 2;
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+
+
+
+        [Test]
+        public void UpCastingFromObjectToSealedTypeThatOverridesAnAbstractMethodThatImplementsAnInterfaceWithAMethodThatIsPureKeepsMethodPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base : IInterface
+{
+    public abstract int Method();
+}
+
+public sealed class Derived : Base
+{
+    public override int Method() => 2;
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+
+        [Test]
+        public void UpCastingFromObjectToSealedTypeThatOverridesAnAbstractMethodWithAMethodThatIsPureAndThatImplementsAnInterfaceKeepsMethodPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base
+{
+    public abstract int Method();
+}
+
+public sealed class Derived : Base, IInterface
+{
+    public override int Method() => 2;
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+
+
+        [Test]
+        public void UpCastingFromObjectToTypeThatOverridesAnAbstractMethodThatImplementsAnInterfaceWithASealedMethodThatIsPureKeepsMethodPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base : IInterface
+{
+    public abstract int Method();
+}
+
+public class Derived : Base
+{
+    public sealed override int Method() => 2;
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+        [Test]
+        public void UpCastingFromObjectToTypeThatOverridesAnAbstractMethodWithASealedMethodThatIsPureAndThatImplementsAnInterfaceKeepsMethodPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base
+{
+    public abstract int Method();
+}
+
+public class Derived : Base, IInterface
+{
+    public sealed override int Method() => 2;
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+
+
+        [Test]
+        public void UpCastingFromObjectToTypeThatOverridesAnAbstractMethodThatImplementsAnInterfaceWithASealedMethodThatIsImpureKeepsMethodPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base : IInterface
+{
+    public abstract int Method();
+}
+
+public class Derived : Base
+{
+    static int state = 0;
+    public sealed override int Method() => state++;
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+
+        [Test]
+        public void UpCastingFromObjectToTypeThatOverridesAnAbstractMethodWithASealedMethodThatIsImpureAndThatImplementsAnInterfaceKeepsMethodPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base
+{
+    public abstract int Method();
+}
+
+public class Derived : Base, IInterface
+{
+    static int state = 0;
+    public sealed override int Method() => state++;
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+
+
+
+        [Test]
+        public void UpCastingFromObjectToTypeWhoseSubTypeOverridesAnAbstractMethodThatImplementsAnInterfaceWithAMethodThatIsImpureKeepsMethodPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base : IInterface
+{
+    public abstract int Method();
+}
+
+public class Middle : Base
+{
+    static int state = 0;
+    public override int Method() => state++;
+}
+
+public class Derived : Middle
+{
+
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+        [Test]
+        public void UpCastingFromObjectToTypeWhoseSubTypeOverridesAnAbstractMethodWithAMethodThatIsImpureAndThatImplementsAnInterfaceKeepsMethodPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base
+{
+    public abstract int Method();
+}
+
+public class Middle : Base, IInterface
+{
+    static int state = 0;
+    public override int Method() => state++;
+}
+
+public class Derived : Middle
+{
+
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+        [Test]
+        public void UpCastingFromObjectToTypeWhoseSubTypeOverridesAnAbstractMethodWithAMethodThatIsImpureAndTypeImplementsAnInterfaceKeepsMethodPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base
+{
+    public abstract int Method();
+}
+
+public class Middle : Base
+{
+    static int state = 0;
+    public override int Method() => state++;
+}
+
+public class Derived : Middle, IInterface
+{
+
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+
+        [Test]
+        public void UpCastingFromObjectToTypeWhoseSubTypeOverridesAnAbstractMethodThatImplementsAnInterfaceWithAMethodThatIsPureMakesMethodImpure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base : IInterface
+{
+    public abstract int Method();
+}
+
+public class Middle : Base
+{
+    public override int Method() => 2;
+}
+
+public class Derived : Middle
+{
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+        [Test]
+        public void UpCastingFromObjectToTypeWhoseSubTypeOverridesAnAbstractMethodWithAMethodThatIsPureAndThatImplementsAnInterfaceMakesMethodImpure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base
+{
+    public abstract int Method();
+}
+
+public class Middle : Base, IInterface
+{
+    public override int Method() => 2;
+}
+
+public class Derived : Middle
+{
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+        [Test]
+        public void UpCastingFromObjectToTypeWhoseSubTypeOverridesAnAbstractMethodWithAMethodThatIsPureAndTypeImplementsAnInterfaceMakesMethodImpure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base
+{
+    public abstract int Method();
+}
+
+public class Middle : Base
+{
+    public override int Method() => 2;
+}
+
+public class Derived : Middle, IInterface
+{
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+
+        [Test]
+        public void UpCastingFromObjectToTypeWhoseSubTypeOverridesAnAbstractMethodThatImplementsAnInterfaceWithASealedMethodThatIsPureKeepsMethodPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base : IInterface
+{
+    public abstract int Method();
+}
+
+public class Middle : Base
+{
+    public sealed override int Method() => 2;
+}
+
+public class Derived : Middle
+{
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+        [Test]
+        public void UpCastingFromObjectToTypeWhoseSubTypeOverridesAnAbstractMethodWithASealedMethodThatIsPureAndThatImplementsAnInterfaceKeepsMethodPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base
+{
+    public abstract int Method();
+}
+
+public class Middle : Base, IInterface
+{
+    public sealed override int Method() => 2;
+}
+
+public class Derived : Middle
+{
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+        [Test]
+        public void UpCastingFromObjectToTypeWhoseSubTypeOverridesAnAbstractMethodWithASealedMethodThatIsPureAndTypeImplementsAnInterfaceKeepsMethodPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base
+{
+    public abstract int Method();
+}
+
+public class Middle : Base
+{
+    public sealed override int Method() => 2;
+}
+
+public class Derived : Middle, IInterface
+{
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+
+        [Test]
+        public void UpCastingFromObjectToTypeWhoseSubTypeOverridesAnAbstractMethodThatImplementsAnInterfaceWithASealedMethodThatIsImpureKeepsMethodPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base : IInterface
+{
+    public abstract int Method();
+}
+
+public class Middle : Base
+{
+    static int state = 0;
+    public sealed override int Method() => state++;
+}
+
+public class Derived : Middle
+{
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+        [Test]
+        public void UpCastingFromObjectToTypeWhoseSubTypeOverridesAnAbstractMethodWithASealedMethodThatIsImpureAndThatImplementsAnInterfaceKeepsMethodPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base
+{
+    public abstract int Method();
+}
+
+public class Middle : Base, IInterface
+{
+    static int state = 0;
+    public sealed override int Method() => state++;
+}
+
+public class Derived : Middle
+{
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+        [Test]
+        public void UpCastingFromObjectToTypeWhoseSubTypeOverridesAnAbstractMethodWithASealedMethodThatIsImpureAndTypeImplementsAnInterfaceKeepsMethodPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public interface IInterface
+{
+    int Method();
+}
+
+public abstract class Base
+{
+    public abstract int Method();
+}
+
+public class Middle : Base
+{
+    static int state = 0;
+    public sealed override int Method() => state++;
+}
+
+public class Derived : Middle, IInterface
+{
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static string DoSomething(object obj)
+    {
+        var v = (Derived)obj;
+
+        return """";
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+    }
+}
