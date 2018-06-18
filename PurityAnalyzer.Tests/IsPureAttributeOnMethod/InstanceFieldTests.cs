100a2829e9a0b76045da49976a5f26902601a7a6
Yacoub Massad <yacoubmassad@gmail.com>
2018-06-18 15:03:43 +0300

    Instance field tests

---
+++ PurityAnalyzer.Tests/IsPureAttributeOnMethod/InstanceFieldTests.cs
@@ -0,0 +1,85 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace PurityAnalyzer.Tests.IsPureAttributeOnMethod
+{
+    [TestFixture]
+    public class InstanceFieldTests
+    {
+        [Test]
+        public void MethodThatReadsAReadonlyInstanceFieldIsPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class Class1
+{
+    readonly int c = 1;
+    [IsPure]
+    public int DoSomething()
+    {
+        return c;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+        [Test]
+        public void MethodThatReadsAReadWriteInstanceFieldIsImpure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class Class1
+{
+    int c = 1;
+    [IsPure]
+    public int DoSomething()
+    {
+        return c;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+        [Test]
+        public void MethodThatWritesAnInstanceFieldIsImpure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class Class1
+{
+    int c = 1;
+    [IsPure]
+    public int DoSomething()
+    {
+        c = 2;
+        return 1;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+    }
+}
