a08b43aab91a8d001c72912fc69bc99c4dc0b565
Yacoub Massad <yacoubmassad@gmail.com>
2018-06-26 23:08:44 +0300

    Adding two tests for accessing sub object of parameter

--- PurityAnalyzer.Tests/IsPureAttributeOnMethod/MethodsOnInputParameterTests.cs
+++ PurityAnalyzer.Tests/IsPureAttributeOnMethod/MethodsOnInputParameterTests.cs
@@ -453,5 +453,79 @@ public static class Module1
             dignostics.Length.Should().BePositive();
         }
 
+        [Test]
+        public void MethodThatInvokesAMethodThatReadsAnReadWriteFieldOnObjectObtainedViaReadOnlyFieldOnParameterWhoseTypeIsDefinedInCodeIsPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class Dto1
+{
+    public int Field = 5;
+    
+    public int Method() => Field;
+}
+
+public class Dto0
+{
+    public readonly Dto1 Dto = new Dto1();
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething(Dto0 input)
+    {
+        return input.Dto.Method();
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+
+            dignostics.Length.Should().Be(0);
+        }
+
+        [Test]
+        public void MethodThatInvokesAMethodThatWritesAnReadWriteFieldOnObjectObtainedViaReadOnlyFieldOnParameterWhoseTypeIsDefinedInCodeIsImpure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class Dto1
+{
+    public int Field = 5;
+    
+    public int Method()
+    {
+        Field = 1;
+        return 1;
+    }
+}
+
+public class Dto0
+{
+    public readonly Dto1 Dto = new Dto1();
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething(Dto0 input)
+    {
+        return input.Dto.Method();
+    }
+}";
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+
+            dignostics.Length.Should().BePositive();
+        }
     }
 }
