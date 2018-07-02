f507be01fbd309e0e3ef570187034be0f341ac6a
Yacoub Massad <yacoubmassad@gmail.com>
2018-07-03 00:05:13 +0300

    Adding tests for custom enumerables

---
+++ PurityAnalyzer.Tests/IsPureAttributeOnMethod/CustomEnumerableTests.cs
@@ -0,0 +1,896 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace PurityAnalyzer.Tests.IsPureAttributeOnMethod
+{
+    [TestFixture]
+    public class CustomEnumerableTests
+    {
+        [Test]
+        public void MethodThatEnumeratesACustomEnumerableAndAllComponentsArePureIsPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class EnumerableClass
+{
+    public EnumeratorClass GetEnumerator() => new EnumeratorClass();
+}
+
+public class EnumeratorClass
+{
+    public int Current => 1;
+
+    public bool MoveNext() => true;
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething()
+    {
+        foreach (var item in new EnumerableClass())
+        {
+        }
+        
+        return 0;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+        [Test]
+        public void MethodThatEnumeratesACustomEnumerableAndTheGetEnumeratorMethodIsImpureIsImpure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class EnumerableClass
+{
+    static int state = 0;
+    public EnumeratorClass GetEnumerator()
+    {   
+        state++;
+        return new EnumeratorClass();
+    }
+}
+
+public class EnumeratorClass
+{
+    public int Current => 1;
+
+    public bool MoveNext() => true;
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething()
+    {
+        foreach (var item in new EnumerableClass())
+        {
+        }
+        
+        return 0;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+        [Test]
+        public void MethodThatEnumeratesACustomEnumerableAndTheCurrentPropertyIsImpureIsImpure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class EnumerableClass
+{
+    public EnumeratorClass GetEnumerator()
+    {   
+        return new EnumeratorClass();
+    }
+}
+
+public class EnumeratorClass
+{
+    static int state = 0;
+
+    public int Current => state++;
+
+    public bool MoveNext() => true;
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething()
+    {
+        foreach (var item in new EnumerableClass())
+        {
+        }
+        
+        return 0;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+        [Test]
+        public void MethodThatEnumeratesACustomEnumerableAndTheMoveNextMethodIsImpureIsImpure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class EnumerableClass
+{
+    public EnumeratorClass GetEnumerator()
+    {   
+        return new EnumeratorClass();
+    }
+}
+
+public class EnumeratorClass
+{
+    static int state = 0;
+
+    public int Current => 1;
+
+    public bool MoveNext() => (state++) == 1;
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething()
+    {
+        foreach (var item in new EnumerableClass())
+        {
+        }
+        
+        return 0;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+        [Test]
+        public void MethodThatEnumeratesACustomEnumerableAndEnumeratorImplmenetsIDisposableAndAllComponentsArePureIsPure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class EnumerableClass
+{
+    public EnumeratorClass GetEnumerator() => new EnumeratorClass();
+}
+
+public class EnumeratorClass : IDisposable
+{
+    public int Current => 1;
+
+    public bool MoveNext() => true;
+
+    public void Dispose(){}
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething()
+    {
+        foreach (var item in new EnumerableClass())
+        {
+        }
+        
+        return 0;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+        [Test]
+        public void MethodThatEnumeratesACustomEnumerableAndEnumeratorImplmenetsIDisposableAndTheDisposeMethodIsImpureIsImpure()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class EnumerableClass
+{
+    public EnumeratorClass GetEnumerator() => new EnumeratorClass();
+}
+
+public class EnumeratorClass : IDisposable
+{
+    public int Current => 1;
+
+    public bool MoveNext() => true;
+
+    static int state = 0;
+
+    public void Dispose()
+    {
+        state++;
+    }
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething()
+    {
+        foreach (var item in new EnumerableClass())
+        {
+        }
+        
+        return 0;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+
+        [Test]
+        public void MethodThatEnumeratesAnEnumerableClassThatImplementsIEnumerableAndAllComponentsArePureIsPure()
+        {
+            string code = @"
+using System;
+using System.Collections;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class EnumerableClass : IEnumerable
+{
+    public IEnumerator GetEnumerator() => new EnumeratorClass();
+}
+
+public class EnumeratorClass : IEnumerator
+{
+    public object Current => 1;
+
+    public bool MoveNext() => true;
+
+    public void Reset()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething()
+    {
+        foreach (var item in new EnumerableClass())
+        {
+        }
+        
+        return 0;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+        [Test]
+        public void MethodThatEnumeratesAnEnumerableClassThatImplementsIEnumerableAndTheGetEnumeratorMethodIsImpureIsImpure()
+        {
+            string code = @"
+using System;
+using System.Collections;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class EnumerableClass : IEnumerable
+{
+    static int state = 0;
+    public IEnumerator GetEnumerator()
+    {   
+        state++;
+        return new EnumeratorClass();
+    }
+}
+
+public class EnumeratorClass : IEnumerator
+{
+    public object Current => 1;
+
+    public bool MoveNext() => true;
+
+    public void Reset()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething()
+    {
+        foreach (var item in new EnumerableClass())
+        {
+        }
+        
+        return 0;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+        [Test]
+        public void MethodThatEnumeratesAnEnumerableClassThatImplementsIEnumerableAndTheCurrentPropertyIsImpureIsImpure()
+        {
+            string code = @"
+using System;
+using System.Collections;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class EnumerableClass : IEnumerable
+{
+    public IEnumerator GetEnumerator()
+    {   
+        return new EnumeratorClass();
+    }
+}
+
+public class EnumeratorClass : IEnumerator
+{
+    static int state = 0;
+
+    public object Current => state++;
+
+    public bool MoveNext() => true;
+
+    public void Reset()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething()
+    {
+        foreach (var item in new EnumerableClass())
+        {
+        }
+        
+        return 0;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+        [Test]
+        public void MethodThatEnumeratesAnEnumerableClassThatImplementsIEnumerableAndTheMoveNextMethodIsImpureIsImpure()
+        {
+            string code = @"
+using System;
+using System.Collections;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class EnumerableClass : IEnumerable
+{
+    public IEnumerator GetEnumerator()
+    {   
+        return new EnumeratorClass();
+    }
+}
+
+public class EnumeratorClass : IEnumerator
+{
+    static int state = 0;
+
+    public object Current => 1;
+
+    public bool MoveNext() => (state++) == 1;
+
+    public void Reset()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething()
+    {
+        foreach (var item in new EnumerableClass())
+        {
+        }
+        
+        return 0;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+        [Test]
+        public void MethodThatEnumeratesAnEnumerableClassThatImplementsIEnumerableAndIDisposableAndAllComponentsArePureIsPure()
+        {
+            string code = @"
+using System;
+using System.Collections;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class EnumerableClass : IEnumerable
+{
+    public IEnumerator GetEnumerator() => new EnumeratorClass();
+}
+
+public class EnumeratorClass : IEnumerator, IDisposable
+{
+    public object Current => 1;
+
+    public bool MoveNext() => true;
+
+    public void Dispose(){}
+
+    public void Reset()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething()
+    {
+        foreach (var item in new EnumerableClass())
+        {
+        }
+        
+        return 0;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+        [Test]
+        public void MethodThatEnumeratesAnEnumerableClassThatImplementsIEnumerableAndIDisposableAndTheDisposeMethodIsImpureIsImpure()
+        {
+            string code = @"
+using System;
+using System.Collections;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class EnumerableClass : IEnumerable
+{
+    public IEnumerator GetEnumerator() => new EnumeratorClass();
+}
+
+public class EnumeratorClass : IEnumerator, IDisposable
+{
+    public object Current => 1;
+
+    public bool MoveNext() => true;
+
+    static int state = 0;
+
+    public void Dispose()
+    {
+        state++;
+    }
+
+    public void Reset()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething()
+    {
+        foreach (var item in new EnumerableClass())
+        {
+        }
+        
+        return 0;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+
+
+
+        [Test]
+        public void MethodThatEnumeratesAnEnumerableClassThatImplementsGenericIEnumerableAndAllComponentsArePureIsPure()
+        {
+            string code = @"
+using System;
+using System.Collections;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+
+
+public class EnumerableClass : IEnumerable<int>
+{
+    public IEnumerator<int> GetEnumerator()
+    {
+        return new EnumeratorClass();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+public class EnumeratorClass : IEnumerator<int>
+{
+    public int Current => 1;
+
+    public bool MoveNext() => true;
+
+    public void Reset()
+    {
+        throw new NotImplementedException();
+    }
+
+    object IEnumerator.Current => Current;
+
+    public void Dispose()
+    {
+    }
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething()
+    {
+        foreach (var item in new EnumerableClass())
+        {
+        }
+        
+        return 0;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+
+        }
+
+        [Test]
+        public void MethodThatEnumeratesAnEnumerableClassThatImplementsGenericIEnumerableAndTheGetEnumeratorMethodIsImpureIsImpure()
+        {
+            string code = @"
+using System;
+using System.Collections;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+
+public class EnumerableClass : IEnumerable<int>
+{
+    static int state = 0;
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        state++;
+        return new EnumeratorClass();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+public class EnumeratorClass : IEnumerator<int>
+{
+    public int Current => 1;
+
+    public bool MoveNext() => true;
+
+    public void Reset()
+    {
+        throw new NotImplementedException();
+    }
+
+    object IEnumerator.Current => Current;
+
+    public void Dispose()
+    {
+    }
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething()
+    {
+        foreach (var item in new EnumerableClass())
+        {
+        }
+        
+        return 0;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+        [Test]
+        public void MethodThatEnumeratesAnEnumerableClassThatImplementsGenericIEnumerableAndTheCurrentPropertyIsImpureIsImpure()
+        {
+            string code = @"
+using System;
+using System.Collections;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class EnumerableClass : IEnumerable<int>
+{
+    public IEnumerator<int> GetEnumerator()
+    {
+        return new EnumeratorClass();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+public class EnumeratorClass : IEnumerator<int>
+{
+    static int state = 0;
+
+    public int Current => state++;
+
+    public bool MoveNext() => true;
+
+    public void Reset()
+    {
+        throw new NotImplementedException();
+    }
+
+    object IEnumerator.Current => Current;
+
+    public void Dispose()
+    {
+    }
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething()
+    {
+        foreach (var item in new EnumerableClass())
+        {
+        }
+        
+        return 0;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+        [Test]
+        public void MethodThatEnumeratesAnEnumerableClassThatImplementsGenericIEnumerableAndTheMoveNextMethodIsImpureIsImpure()
+        {
+            string code = @"
+using System;
+using System.Collections;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class EnumerableClass : IEnumerable<int>
+{
+    public IEnumerator<int> GetEnumerator()
+    {
+        return new EnumeratorClass();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+public class EnumeratorClass : IEnumerator<int>
+{
+    static int state = 0;
+
+    public int Current => 1;
+
+    public bool MoveNext() => (state++) == 1;
+
+    public void Reset()
+    {
+        throw new NotImplementedException();
+    }
+
+    object IEnumerator.Current => Current;
+
+    public void Dispose()
+    {
+    }
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething()
+    {
+        foreach (var item in new EnumerableClass())
+        {
+        }
+        
+        return 0;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+        [Test]
+        public void MethodThatEnumeratesAnEnumerableClassThatImplementsGenericIEnumerableAndIDisposableAndTheDisposeMethodIsImpureIsImpure()
+        {
+            string code = @"
+using System;
+using System.Collections;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class EnumerableClass : IEnumerable<int>
+{
+    public IEnumerator<int> GetEnumerator()
+    {
+        return new EnumeratorClass();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+public class EnumeratorClass : IEnumerator<int>
+{
+    static int state = 0;
+
+    public int Current => 1;
+
+    public bool MoveNext() => true;
+
+    public void Reset()
+    {
+        throw new NotImplementedException();
+    }
+
+    object IEnumerator.Current => Current;
+
+    public void Dispose()
+    {
+        state++;
+    }
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static int DoSomething()
+    {
+        foreach (var item in new EnumerableClass())
+        {
+        }
+        
+        return 0;
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().BePositive();
+
+        }
+
+    }
+}
