65e63f4d26dd24e8d3e66d8fceba0b2fcc6683d9
Yacoub Massad <yacoubmassad@gmail.com>
2018-11-02 22:53:31 +0200

    Started working on calling generic methods

---
+++ PurityAnalyzer.Tests/IsPureAttributeOnMethod/CallingGenericMethods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace PurityAnalyzer.Tests.IsPureAttributeOnMethod
+{
+    [TestFixture]
+    public class CallingGenericMethods
+    {
+        [Test]
+        public void CallingSimpleGenericMethod()
+        {
+            string code = @"
+using System;
+
+public class IsPureAttribute : Attribute
+{
+}
+
+public class Class1 
+{
+}
+
+public static class Module1
+{
+    [IsPure]
+    public static void DoSomething()
+    {
+        GenericMethod<Class1>(new Class1());
+    }
+
+    public static void GenericMethod<T>(T param)
+    {
+
+    }
+}";
+
+            var dignostics = Utilities.RunPurityAnalyzer(code);
+            dignostics.Length.Should().Be(0);
+        }
+    }
+}
